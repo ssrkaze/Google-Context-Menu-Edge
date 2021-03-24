5f81114990b451acf19b434e17881ad7873ccea8
ssrkaze <Mr_kaze@outlook.com>
2021-03-24 22:28:54 +0800

    fix search link

--- Google Search/background.js
+++ Google Search/background.js
@@ -1,14 +1,13 @@
-
 function onRequest(info, tab) {
-	var selection = info.selectionText;
-//do something with the selection
-  var serviceCall = 'https://www.google.com/#q=' + encodeURIComponent(selection);
-  chrome.tabs.create({url: serviceCall});
-};
+    var selection = info.selectionText;
+    //do something with the selection
+    var serviceCall = "https://google.com/search?q=" + encodeURIComponent(selection);
+    chrome.tabs.create({ url: serviceCall });
+}
 
 chrome.contextMenus.create({
-	id: "google_search",
-	title: "Search with Google",
-	"contexts":["selection"],
-	"onclick" : onRequest
+    id: "google_search",
+    title: "Search with Google",
+    contexts: ["selection"],
+    onclick: onRequest,
 });
